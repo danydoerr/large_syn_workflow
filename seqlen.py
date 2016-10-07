943c3c9d287715c5f91dff72cf1055b2c8e2b901
Daniel Doerr <daniel.doerr@epfl.ch>
2016-10-07 16:00:27 +0200

    remove obsolete option for "inactive" sequences

--- seqlen.py
+++ seqlen.py
@@ -8,10 +8,6 @@ if __name__ == '__main__':
 
     usage = 'usage: %prog [options] <FASTA FILE>'
     parser = OptionParser(usage=usage)
-    parser.add_option('-i', '--ignore_inactive', dest='ignoreInactive',
-            help='Ignore fasta records that have been inactivated ' + \
-                    'through the [inactivate] tag', action='store_true',
-                    default=False) 
     parser.add_option('-c', '--cumulative', dest='cumulative',
             help='Print sum length of fasta records', action='store_true',
             default=False) 
@@ -24,11 +20,9 @@ if __name__ == '__main__':
 
     c = 0
     for rec in SeqIO.parse(args[0], 'fasta'):
-        if not options.ignoreInactive or \
-                not rec.description.endswith('[inactivate]'):
-            if options.cumulative:
-                c += len(rec)
-            else:
-                print '%s\t%s' %(rec.id, len(rec))
+        if options.cumulative:
+            c += len(rec)
+        else:
+            print '%s\t%s' %(rec.id, len(rec))
     if options.cumulative:
         print c
